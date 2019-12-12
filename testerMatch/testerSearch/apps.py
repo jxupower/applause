e2ecdb5e141d57cf3add6898e993afc7d18f1783
Jack Xu <jackhxu16@gmail.com>
2019-12-12 17:09:03 -0500

    Add: Django basic structure

---
+++ testerMatch/testerSearch/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TestersearchConfig(AppConfig):
+    name = 'testerSearch'
---
+++ testerMatch/testerSearch/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+def home(request):
+    return render(request, 'testerSearch/home.html')
+
+
+def requirements(request):
+    return render(request, 'testerSearch/requirements.html')
+
