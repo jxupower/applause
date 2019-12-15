b25b1edd538d1bf6d226c23aaab6361fb379160d
Jack Xu <jackhxu16@gmail.com>
2019-12-15 07:37:55 -0500

    Add: Search Query

--- testerMatch/testerSearch/views.py
+++ testerMatch/testerSearch/views.py
@@ -1,10 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Tester, Device, Bug
+
 
 # Create your views here.
 
 def home(request):
-    return render(request, 'testerSearch/home.html')
+    exp = filter(request)
+    devices = Device.objects.all()
+    countries = []
+    for t in Tester.objects.all():
+        if t.country not in countries:
+            countries.append(t.country)
+    context = {
+        'experience': exp.items(),
+        'countries': countries,
+        'devices': devices,
+    }
+    return render(request, 'testerSearch/home.html', context)
 
 
 def requirements(request):
@@ -12,5 +25,18 @@ def requirements(request):
 
 
 
-
-
+def filter(request):
+    country = request.GET.get('countries')
+    device = request.GET.get('devices')
+    qs = Tester.objects.all()
+    if country != "ALL":
+        qs = qs.filter(country=country)
+    if device != "ALL":
+        qs = qs.filter(device=Device.objects.get(deviceName=device))
+    #bugs = Bug.objects.select_related("tester__device").filter(tester)
+    exp = {}
+    for tester in qs:
+        bugs = Bug.objects.all()
+        bugs = bugs.filter(tester=tester.id)
+        exp[tester] = bugs.count()
+    return exp
